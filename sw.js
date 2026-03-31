53559c3f143d97b330a239a711c52dd86a0ce54d
Andriy Gavrylenko <gavrosh2007@gmail.com>
2026-03-31 09:42:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,14 +2,20 @@ const CACHE_NAME = 'hermes-v1';
 const urlsToCache = [
   './',
   './index.html',
-  'dervish.mp4',
-  'inspire.mp3'
+  './manifest.json',
+  './dervish.mp4',
+  './inspire.mp3',
+  './icon-48x48.png',
+  './icon-96x96.png',
+  './icon-192x192.png',
+  './icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
@@ -28,4 +34,5 @@ self.addEventListener('activate', event => {
       })
     ))
   );
+  self.clients.claim();
 });
